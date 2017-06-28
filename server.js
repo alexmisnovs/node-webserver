f1a776cdd7f67f37bb37e06fed04874e2b6ddb83
Alex Misnovs <alexmisnovs@Alexs-iMac.local>
2017-06-28 10:13:27 +0100

    setup start script and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000; // to work on heroku
 
 var app = express();
 // import hbs tempalating
@@ -81,6 +82,9 @@ app.get('/bad', (req, res) => {
         }
     );
 });
-app.listen(3000, () => {
-    console.log("server is up on port 3000");
+// need to pass dynamic port in environment variable
+
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
 });
